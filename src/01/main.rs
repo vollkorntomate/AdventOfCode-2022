f7dc210e1740a809770d86d4ca62e0fc09850352
vollkorntomate <thomas@vollkorntomate.de>
2022-12-01 09:21:39 +0100

    Day 1, Part 2

--- src/01/main.rs
+++ src/01/main.rs
@@ -1,10 +1,10 @@
-use std::{fs};
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("src/01/input.txt").expect("File not found");
-    
+
     let mut calories: Vec<u64> = vec![];
-    
+
     let mut sum = 0u64;
     for line in input.lines() {
         if line.is_empty() {
@@ -15,12 +15,17 @@ fn main() {
         sum += line.parse::<u64>().expect("Not a number!");
     }
 
-    let mut max = (0usize, 0u64);
-    for cal in calories.iter().enumerate() {
-        if cal.1 > &max.1 {
-            max = (cal.0, *cal.1);
-        }
-    }
+    calories.sort();
+    calories.reverse();
 
-    println!("Max value is {} for elf n° {}.", max.1, max.0);
+    let top = (
+        calories.get(0).unwrap(),
+        calories.get(1).unwrap(),
+        calories.get(2).unwrap(),
+    );
+    let top_sum = top.0 + top.1 + top.2;
+    println!(
+        "The top three have {}, {} and {} respectively, sum: {}",
+        top.0, top.1, top.2, top_sum
+    );
 }
