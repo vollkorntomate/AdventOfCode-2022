698e3ea4c2e57ba4f33a6818729bb9d5f02656c1
vollkorntomate <thomas@vollkorntomate.de>
2022-12-08 22:20:41 +0100

    Day 8, Part 2

--- src/08/main.rs
+++ src/08/main.rs
@@ -4,8 +4,10 @@ fn main() {
     let input = fs::read_to_string("src/08/input.txt").expect("File not found");
 
     let num_visible = num_visible_trees(input.as_str()).unwrap();
+    let scenic_score = scenic_score(input.as_str()).unwrap();
 
     println!("Number of visible trees: {}", num_visible);
+    println!("Best scenic score: {}", scenic_score);
 }
 
 fn num_visible_trees(input: &str) -> Option<u64> {
@@ -44,6 +46,46 @@ fn num_visible_trees(input: &str) -> Option<u64> {
     Some(count + num_outside(&matrix)?)
 }
 
+fn scenic_score(input: &str) -> Option<u64> {
+    let matrix = parse_input(input);
+
+    let mut max_score = 0u64;
+    for y in 1..matrix.len() - 1 {
+        for x in 1..matrix.first()?.len() - 1 {
+            let height = matrix.get(y)?.get(x)?;
+            let mut scores = [0u64; 4];
+            for yy in (0..y).rev() {
+                scores[0] += 1;
+                if matrix.get(yy)?.get(x)? >= height {
+                    break;
+                }
+            }
+            for yy in y + 1..matrix.len() {
+                scores[1] += 1;
+                if matrix.get(yy)?.get(x)? >= height {
+                    break;
+                }
+            }
+            for xx in (0..x).rev() {
+                scores[2] += 1;
+                if matrix.get(y)?.get(xx)? >= height {
+                    break;
+                }
+            }
+            for xx in x + 1..matrix.first()?.len() {
+                scores[3] += 1;
+                if matrix.get(y)?.get(xx)? >= height {
+                    break;
+                }
+            }
+            let score = scores.into_iter().reduce(|acc, s| acc * s)?;
+            max_score = score.max(max_score);
+        }
+    }
+
+    Some(max_score)
+}
+
 fn num_outside(matrix: &Vec<Vec<u8>>) -> Option<u64> {
     let rows = matrix.len();
     let cols = matrix.first()?.len();
@@ -70,5 +112,10 @@ fn test() {
                         33549\n\
                         35390";
 
+    for x in 2..=0 {
+        println!("{x}");
+    }
+
     assert_eq!(num_visible_trees(input), Some(21));
+    assert_eq!(scenic_score(input), Some(8));
 }
