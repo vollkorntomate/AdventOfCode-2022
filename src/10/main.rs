9dd27a54f28d607ff65c363c23fa21633ea3165f
vollkorntomate <thomas@vollkorntomate.de>
2022-12-10 23:28:57 +0100

    Day 10, Part 2

--- src/10/main.rs
+++ src/10/main.rs
@@ -17,6 +17,8 @@ enum Op {
 struct State {
     cycle: i64,
     x: i64,
+    crt: usize,
+    pixels: [char; 240],
 }
 
 const STOPS: [i64; 6] = [20, 60, 100, 140, 180, 220];
@@ -34,19 +36,28 @@ fn sum_signal_strengths(input: &str) -> i64 {
         };
     }
 
+    state.print_screen();
+
     sum
 }
 
 impl State {
     fn new() -> State {
-        State { cycle: 0, x: 1 }
+        State {
+            cycle: 0,
+            x: 1,
+            crt: 0,
+            pixels: ['.'; 240],
+        }
     }
 
     fn do_op(&mut self, op: Op) -> i64 {
+        self.draw_pixel();
         self.cycle += 1;
         let mut signal_strength = self.check_signal_strength();
 
         if let Op::Addx(add) = op {
+            self.draw_pixel();
             self.cycle += 1;
 
             if signal_strength == 0 {
@@ -64,6 +75,23 @@ impl State {
             self.cycle * self.x
         } else {
             0
+        }
+    }
+
+    fn draw_pixel(&mut self) {
+        let offset_x = self.x + (self.crt as i64 / 40) * 40;
+        if self.crt as i64 >= offset_x - 1 && self.crt as i64 <= offset_x + 1 {
+            self.pixels[self.crt] = '#';
+        }
+        self.crt += 1;
+    }
+
+    fn print_screen(&self) {
+        for (i, pixel) in self.pixels.iter().enumerate() {
+            print!("{}", pixel);
+            if (i + 1) % 40 == 0 {
+                print!("\n");
+            }
         }
     }
 }
