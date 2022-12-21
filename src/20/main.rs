cfa14ab8e9fea0dc9c8e5e2dc8de3ad41f4c2f97
vollkorntomate <thomas@vollkorntomate.de>
2022-12-21 12:47:47 +0100

    Day 20, Part 1

---
+++ src/20/main.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("src/20/input.txt").unwrap();
+
+    let coordinate_sum = mix(input.as_str());
+
+    println!("The sum of the coordinates is {coordinate_sum}");
+}
+
+fn mix(input: &str) -> i32 {
+    let original = input
+        .split("\n")
+        .filter(|&s| !s.trim().is_empty())
+        .map(|s| s.trim().parse::<i32>().unwrap())
+        .enumerate()
+        .collect::<Vec<_>>();
+    let mut mixed = original.clone();
+
+    for n in original.iter() {
+        let old_index = mixed.iter().position(|x| x == n).unwrap();
+        let removed = mixed.remove(old_index);
+        
+        let size = mixed.len() as i32;
+        let new_index = (old_index as i32 + n.1).rem_euclid(size) as usize;
+        
+        mixed.insert(new_index, removed);
+    }
+
+    let zero_index = mixed.iter().position(|(_, n)| *n == 0).unwrap();
+    let (first, second, third) = (
+        mixed[(zero_index + 1000) % mixed.len()].1,
+        mixed[(zero_index + 2000) % mixed.len()].1,
+        mixed[(zero_index + 3000) % mixed.len()].1
+    );
+
+    first + second + third
+}
+
+#[test]
+fn test() {
+    let input = "1\n\
+                2\n\
+                -3\n\
+                3\n\
+                -2\n\
+                0\n\
+                4";
+
+    mix(input);
+
+    assert_eq!(mix(input), 3);
+}
