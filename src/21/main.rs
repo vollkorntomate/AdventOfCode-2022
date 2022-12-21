0bba4f2dd324f86ee98219b8e7b91de8e95e9ff0
vollkorntomate <thomas@vollkorntomate.de>
2022-12-21 17:59:19 +0100

    Day 21, Part 2

--- src/21/main.rs
+++ src/21/main.rs
@@ -3,35 +3,80 @@ use std::{collections::HashMap, fs};
 fn main() {
     let input = fs::read_to_string("src/21/input.txt").unwrap();
 
-    let root_yell = calc_root_yell(input.as_str());
+    let human_yell = calc_human_yell(input.as_str());
 
-    println!("The root monkey yells the number {root_yell}");
+    println!("You have to yell {human_yell}");
 }
 
 type YellMap<'a> = HashMap<&'a str, Yell<'a>>;
 
-fn calc_root_yell<'a>(input: &'a str) -> i64 {
+fn calc_human_yell<'a>(input: &'a str) -> i64 {
     let yells = parse(input);
 
-    calc_yell_rec(&yells, "root")
+    let root = yells.get("root").unwrap();
+    if let Yell::Op((left, right, _)) = root {
+        let root_left = calc_yell_rec(&yells, left);
+        let root_right = calc_yell_rec(&yells, right);
+
+        if let Some(search) = root_left {
+            return calc_human_yell_rec(&yells, right, search);
+        } else {
+            return calc_human_yell_rec(&yells, left, root_right.unwrap());
+        }
+    }
+    0
 }
 
-fn calc_yell_rec(yells: &YellMap, monkey: &str) -> i64 {
+fn calc_yell_rec(yells: &YellMap, monkey: &str) -> Option<i64> {
+    if monkey == "humn" {
+        return None;
+    }
     match yells.get(monkey).unwrap() {
-        Yell::Number(num) => *num,
+        Yell::Number(num) => Some(*num),
         Yell::Op((left_monkey, right_monkey, op)) => {
             let left = calc_yell_rec(yells, left_monkey);
             let right = calc_yell_rec(yells, right_monkey);
 
             match op {
-                '+' => left + right,
-                '-' => left - right,
-                '*' => left * right,
-                '/' => left / right,
+                '+' => Some(left? + right?),
+                '-' => Some(left? - right?),
+                '*' => Some(left? * right?),
+                '/' => Some(left? / right?),
                 _ => panic!("Unexpected operator"),
             }
         }
     }
+}
+
+fn calc_human_yell_rec(yells: &YellMap, monkey: &str, search: i64) -> i64 {
+    if let Yell::Op((left, right, op)) = yells.get(monkey).unwrap() {
+        let left_val = calc_yell_rec(yells, left);
+        let right_val = calc_yell_rec(yells, right);
+
+        if let Some(val) = left_val {
+            return match op {
+                '+' => calc_human_yell_rec(yells, right, search - val),
+                '-' => calc_human_yell_rec(yells, right, val - search),
+                '*' => calc_human_yell_rec(yells, right, search / val),
+                '/' => calc_human_yell_rec(yells, right, val / search),
+                _ => panic!("Unexpected operator"),
+            };
+        } else if let Some(val) = right_val {
+            return match op {
+                '+' => calc_human_yell_rec(yells, left, search - val),
+                '-' => calc_human_yell_rec(yells, left, search + val),
+                '*' => calc_human_yell_rec(yells, left, search / val),
+                '/' => calc_human_yell_rec(yells, left, search * val),
+                _ => panic!("Unexpected operator"),
+            };
+        } else {
+            // None, only for "humn"
+            println!("None found for monkey {monkey}, seach = {search}");
+            return search;
+        }
+    }
+
+    search
 }
 
 fn parse<'a>(input: &'a str) -> YellMap {
@@ -82,5 +127,5 @@ fn test() {
                     drzm: hmdt - zczc\n\
                     hmdt: 32";
 
-    assert_eq!(calc_root_yell(input), 152);
+    assert_eq!(calc_human_yell(input), 301);
 }
